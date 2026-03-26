5147c47a85fe91c226199cb65dccdbeba2fbd70a
jamil <developerlutfur@gmail.com>
2026-03-27 02:13:57 +0600

    Global UI and Responsive with Dimen

---
+++ scalable-dimen/src/main/java/com/jamil/baseui/JamilBaseUI.java
@@ -0,0 +1,14 @@
+package com.jamil.baseui;
+
+/**
+ * JamilBaseUI Library Core
+ * This class provides a centralized entry point for the JamilBaseUI library resources.
+ * Developed by Jamil Lab LTD.
+ */
+public class JamilBaseUI {
+    public static final String VERSION = "1.0.0";
+    
+    private JamilBaseUI() {
+        // Private constructor to prevent instantiation
+    }
+}
